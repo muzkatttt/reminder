c287ba8bd0e5c30584593ace7570f8051c0aa1ad
Katerina <emrodionova@bk.ru>
2025-01-23 23:20:24 +0300

    delete all & create entities

--- src/main/java/com/muzkat/reminder/model/Remind.java
+++ src/main/java/com/muzkat/reminder/model/Remind.java
@@ -1,73 +1,76 @@
 package com.muzkat.reminder.model;
-import org.springframework.stereotype.Component;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-@Component
+
+/**
+ * Класс напоминания со свойствами <b>id</b>, <b>title</b>, <b>description</b>, <b>dateTimeOfRemind</b>.
+ *  @author ekaterinarodionova
+ */
+
+@Entity
+@Data
+@Table(name= "reminds")
 public class Remind {
-    private UUID id; // уникальный идентификатор
-    private String title; // краткое описание
-    private String description; // полное описание
-    private LocalDateTime remind; // дата и время напоминания
-
-
-    public Remind() {
-        super();
-    }
-
-    public Remind(UUID id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        remind = LocalDateTime.now().plusHours(12);
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getRemind() {
-        return remind;
-    }
-
-    public void setRemind(LocalDateTime remind) {
-        this.remind = remind;
-    }
 
 
     /**
-     * Метод проверки валидности при создании напоминания
-     * @return true объект создан без пустых полей
-     * false не валидный объект
+     * Поле уникальный идентификатор напоминания
      */
-    public boolean doCheckCreateReminder(){
-        return (title != null && !title.isEmpty())
-                && (description != null && !description.isEmpty());
+    @Id
+    @Column(name = "remind_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long remindId;
+
+
+    /**
+     * Поле краткое описание напоминания
+     */
+    @Column(name="title")
+    private String title;
+
+
+    /**
+     * Поле полное описание напоминания
+     */
+    @Column(name="description")
+    private String description;
+
+
+    /**
+     * Поле дата и время напоминания
+     */
+    @Column(name="remind")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateTimeOfRemind;
+
+
+    /**
+     * Поле идентификатор пользователя
+     */
+    @Column(name="user_id")
+    private long userId;
+
+
+    /**
+     * Конструктор - создание нового объекта
+     * @param title краткое описание
+     * @param description полное описание
+     * @param dateTimeOfRemind дата и время отправки напоминания пользователю
+     * @param userId идентификатор пользователя
+     */
+    public Remind(String title, String description, LocalDateTime dateTimeOfRemind, long userId) {
+        this.title = title;
+        this.description = description;
+        this.dateTimeOfRemind = dateTimeOfRemind;
+        this.userId = userId;
     }
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Reminder [UUID=").append(id)
-                .append(", title=").append(title)
-                .append(", description=").append(description)
-                .append(", dateRemind=").append(remind);
-        return stringBuilder.toString();
-    }
+
 }
--- src/main/java/com/muzkat/reminder/model/User.java
+++ src/main/java/com/muzkat/reminder/model/User.java
@@ -1,30 +1,54 @@
 package com.muzkat.reminder.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import org.springframework.stereotype.Component;
-
+import lombok.Data;
 import java.util.UUID;
 
-@Component
-@Table(name="user")
+/**
+ * Класс пользователя с полями <b>id</b>, <b>name</b>, <b>userEmail</b>
+ * @author ekaterinarodionova
+ */
+
+@Entity
+@Data
+@Table(name="users")
 public class User {
+
+    /**
+     * Поле уникальный идентификатор пользователя
+     */
     @Id
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID user_id;
+    private long id;
+
+
+    /**
+     * Поле имя пользователя
+     */
+    @Column(name = "name")
     private String name;
-    private String user_email;
 
-    public User(UUID user_id, String name) {
-        this.user_id = user_id;
-        this.name = name;
-    }
 
-    public User(UUID user_id, String name, String user_email) {
-        this.user_id = user_id;
+    /**
+     * Поле адрес электронной почты пользователя
+     */
+    @Column(name="user_email")
+    private String userEmail;
+
+
+    /**
+     * Конструктор - создание нового объекта
+     * @param name имя пользователя
+     * @param userEmail электронный адрес пользователя
+     */
+    public User(String name, String userEmail) {
         this.name = name;
-        this.user_email = user_email;
+        this.userEmail = userEmail;
     }
 }
